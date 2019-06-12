fc8b62fad417aff70264cc87662b7f2fa0e23c46
irbees2008 <irbees2008@gmail.com>
2019-06-12 12:36:18 +0600

    правка

--- engine/plugins/qrcode/config.php
+++ engine/plugins/qrcode/config.php
@@ -12,22 +12,22 @@ pluginsLoadConfig();
 	
 // Fill configuration parameters
 $cfg = array();
-array_push($cfg, array('descr' => 'Р”Р°РЅРЅС‹Р№ РїР»Р°РіРёРЅ РіРµРЅРµСЂРёСЂСѓРµС‚ QRcode'));
+array_push($cfg, array('descr' => 'Данный плагин генерирует QRcode'));
 
 $cfgX = array();     
-    array_push($cfgX, array('name' => 'chs', 'title' => 'Р Р°Р·РјРµСЂС‹ РІ РїРёРєСЃРµР»СЏС…', 'type' => 'input', 'value' => intval(pluginGetVariable($plugin,'chs'))?pluginGetVariable($plugin,'chs'):'150'));
-	array_push($cfgX, array('name' => 'chld', 'title' => 'РЈСЂРѕРІРµРЅСЊ РєРѕСЂСЂРµРєС†РёРё РѕС€РёР±РѕРє<br /><small><b>L</b> - Allows recovery of up to 7% data loss (<b>РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ</b>)<br /><b>M</b> - Allows recovery of up to 15% data loss<br /><b>Q</b> - Allows recovery of up to 25% data loss<br /><b>H</b> - Allows recovery of up to 30% data loss</small>', 'type' => 'select', 'values' => array ( 'L' => 'L', 'M' => 'M', 'Q' => 'Q', 'H' => 'H'), 'value' => pluginGetVariable($plugin,'chld')));
-    array_push($cfgX, array('name' => 'margin', 'title' => 'РћС‚СЃС‚СѓРї', 'type' => 'input', 'value' => intval(pluginGetVariable($plugin,'margin'))?pluginGetVariable($plugin,'margin'):'4'));	
-   	array_push($cfgX, array('name' => 'upload', 'title' => 'Р—Р°РіСЂСѓР¶Р°С‚СЊ QRcode РЅР° СЃР°Р№С‚', 'type' => 'checkbox', 'value' => pluginGetVariable($plugin,'upload')));	
-array_push($cfg,  array('mode' => 'group', 'title' => '<b>РћСЃРЅРѕРІРЅС‹Рµ РЅР°СЃС‚СЂРѕР№РєРё</b>', 'entries' => $cfgX));
+    array_push($cfgX, array('name' => 'chs', 'title' => 'Размеры в пикселях', 'type' => 'input', 'value' => intval(pluginGetVariable($plugin,'chs'))?pluginGetVariable($plugin,'chs'):'150'));
+	array_push($cfgX, array('name' => 'chld', 'title' => 'Уровень коррекции ошибок<br /><small><b>L</b> - Allows recovery of up to 7% data loss (<b>по умолчанию</b>)<br /><b>M</b> - Allows recovery of up to 15% data loss<br /><b>Q</b> - Allows recovery of up to 25% data loss<br /><b>H</b> - Allows recovery of up to 30% data loss</small>', 'type' => 'select', 'values' => array ( 'L' => 'L', 'M' => 'M', 'Q' => 'Q', 'H' => 'H'), 'value' => pluginGetVariable($plugin,'chld')));
+    array_push($cfgX, array('name' => 'margin', 'title' => 'Отступ', 'type' => 'input', 'value' => intval(pluginGetVariable($plugin,'margin'))?pluginGetVariable($plugin,'margin'):'4'));	
+   	array_push($cfgX, array('name' => 'upload', 'title' => 'Загружать QRcode на сайт', 'type' => 'checkbox', 'value' => pluginGetVariable($plugin,'upload')));	
+array_push($cfg,  array('mode' => 'group', 'title' => '<b>Основные настройки</b>', 'entries' => $cfgX));
 
 $cfgX = array();
-	array_push($cfgX, array('name' => 'localsource', 'title' => 'Р’С‹Р±РµСЂРёС‚Рµ РєР°С‚Р°Р»РѕРі РёР· РєРѕС‚РѕСЂРѕРіРѕ РїР»Р°РіРёРЅ Р±СѓРґРµС‚ Р±СЂР°С‚СЊ С€Р°Р±Р»РѕРЅС‹ РґР»СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ<br /><small><b>РЁР°Р±Р»РѕРЅ СЃР°Р№С‚Р°</b> - РїР»Р°РіРёРЅ Р±СѓРґРµС‚ РїС‹С‚Р°С‚СЊСЃСЏ РІР·СЏС‚СЊ С€Р°Р±Р»РѕРЅС‹ РёР· РѕР±С‰РµРіРѕ С€Р°Р±Р»РѕРЅР° СЃР°Р№С‚Р°; РІ СЃР»СѓС‡Р°Рµ РЅРµРґРѕСЃС‚СѓРїРЅРѕСЃС‚Рё - С€Р°Р±Р»РѕРЅС‹ Р±СѓРґСѓС‚ РІР·СЏС‚С‹ РёР· СЃРѕР±СЃС‚РІРµРЅРЅРѕРіРѕ РєР°С‚Р°Р»РѕРіР° РїР»Р°РіРёРЅР°<br /><b>РџР»Р°РіРёРЅ</b> - С€Р°Р±Р»РѕРЅС‹ Р±СѓРґСѓС‚ Р±СЂР°С‚СЊСЃСЏ РёР· СЃРѕР±СЃС‚РІРµРЅРЅРѕРіРѕ РєР°С‚Р°Р»РѕРіР° РїР»Р°РіРёРЅР°</small>', 'type' => 'select', 'values' => array ( '0' => 'РЁР°Р±Р»РѕРЅ СЃР°Р№С‚Р°', '1' => 'РџР»Р°РіРёРЅ'), 'value' => intval(pluginGetVariable($plugin,'localsource'))));
-array_push($cfg,  array('mode' => 'group', 'title' => '<b>РќР°СЃС‚СЂРѕР№РєРё РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ</b>', 'entries' => $cfgX));
+	array_push($cfgX, array('name' => 'localsource', 'title' => 'Выберите каталог из которого плагин будет брать шаблоны для отображения<br /><small><b>Шаблон сайта</b> - плагин будет пытаться взять шаблоны из общего шаблона сайта; в случае недоступности - шаблоны будут взяты из собственного каталога плагина<br /><b>Плагин</b> - шаблоны будут браться из собственного каталога плагина</small>', 'type' => 'select', 'values' => array ( '0' => 'Шаблон сайта', '1' => 'Плагин'), 'value' => intval(pluginGetVariable($plugin,'localsource'))));
+array_push($cfg,  array('mode' => 'group', 'title' => '<b>Настройки отображения</b>', 'entries' => $cfgX));
 
 $cfgX = array();
-	array_push($cfgX, array('name' => 'clear_qrcode', 'title' => 'РЈРґР°Р»РёС‚СЊ РЅРµРёСЃРїРѕР»СЊР·СѓРµРјС‹Рµ РєСЌС€ Рё QRcode', 'type' => 'select', 'value' => 0, 'values' => array ( 0 => $lang['noa'], 1 => $lang['yesa']), 'nosave' => 1));
-array_push($cfg,  array('mode' => 'group', 'title' => '<b>РћС‡РёСЃС‚РєР° СЃРёСЃС‚РµРјС‹</b>', 'entries' => $cfgX));
+	array_push($cfgX, array('name' => 'clear_qrcode', 'title' => 'Удалить неиспользуемые кэш и QRcode', 'type' => 'select', 'value' => 0, 'values' => array ( 0 => $lang['noa'], 1 => $lang['yesa']), 'nosave' => 1));
+array_push($cfg,  array('mode' => 'group', 'title' => '<b>Очистка системы</b>', 'entries' => $cfgX));
 
 // RUN 
 if ($_REQUEST['action'] == 'commit') {
@@ -61,6 +61,6 @@ function clear_qrcode() {
 			}
 		}
 	}
-	msg(array('type' => 'info', 'info' => 'РќРµРёСЃРїРѕР»СЊР·СѓРµРјС‹Рµ QRcode СѓРґР°Р»РµРЅС‹'));
-	msg(array('type' => 'info', 'info' => 'РљСЌС€ РѕС‡РёС‰РµРЅ'));
+	msg(array('type' => 'info', 'info' => 'Неиспользуемые QRcode удалены'));
+	msg(array('type' => 'info', 'info' => 'Кэш очищен'));
 }
